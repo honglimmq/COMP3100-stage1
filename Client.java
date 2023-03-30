3d730717019f820e1829e724d36bb2cd7a5af434
Moon <hong.lim@students.mq.edu.au>
2023-03-30 20:04:07 +1100

    -Implemented logic for retrieving and process information after GETS command is used\n -Modified sendMsg(Command, String) such that it handles empty string properlly\n \t This was achieved my creating two conditional branches so that it does not add whitespace and breakline incorrectly\n -Added a copy of executable ds-server from ds-sim, as client needs ds-server in the same folder to run correctly\n\0

--- Client.java
+++ Client.java
@@ -1,6 +1,7 @@
 import java.net.*;
 import Client.Command;
 import java.io.*;
+import java.util.*;
 
 enum Command {
   HELO, AUTH, REDY, OK, GETS, SCHD, ENQJ, DEQJ, LSTQ, CNTJ, EJWT, LSTJ, MIGJ, KILJ, TERM, QUIT
@@ -31,8 +32,11 @@ public class Client {
   int reqMemory;
   int reqDisk;
 
+  // server information
+  
+
   public void run() throws IOException {
-    // Connect 
+    // Connect
     socket = new Socket("localhost", SERVERPORT);
     out = new DataOutputStream(socket.getOutputStream());
     in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -40,32 +44,42 @@ public class Client {
     // TCP handshake
     sendMsg(Command.HELO);
     recvMsg();
-    sendMsg(Command.AUTH, " Ty");
+    sendMsg(Command.AUTH, "Ty");
     recvMsg();
 
-    while(incomingMsg.equals(ServerCommand.NONE.toString()) && !incomingMsg.isEmpty()){
+    while (!incomingMsg.equals(ServerCommand.NONE.toString())) {
       // Signal server for a job
       sendMsg(Command.REDY);
       incomingMsg = recvMsg();
 
       String[] tokens = incomingMsg.split("\\s+");
-      ServerCommand cmd = ServerCommand.valueOf(tokens[0]);
+      ServerCommand recvCommand = ServerCommand.valueOf(tokens[0]);
 
-      switch(cmd){
+      switch (recvCommand) {
         case JOBN:
           parseJobInfo(tokens);
 
-          
+
           break;
-        case JOBP:
-          break;
-        case NONE:
-          break;
-      } 
+      }
+
+      // Generate GETS command
+      String parametersForGETS =
+          "Capable" + WHITESPACE + reqCore + WHITESPACE + reqMemory + WHITESPACE + reqDisk;
+
+      sendMsg(Command.GETS, parametersForGETS);
+      incomingMsg = recvMsg();
+      tokens = incomingMsg.split("\\s++");
+      int numOfServer = Integer.parseInt(tokens[1]);
+
+      sendMsg(Command.OK);
 
       
 
-      
+
+
+
+
     }
 
 
@@ -89,7 +103,12 @@ public class Client {
 
 
   void sendMsg(Command cmd, String parameters) throws IOException {
-    String message = cmd + parameters + BREAKLINE;
+    String message;
+    if(!parameters.isEmpty()){
+      message = cmd + WHITESPACE + parameters + BREAKLINE;
+    } else {
+      message = cmd + BREAKLINE;
+    }
 
     out.write(message.getBytes());
     out.flush();
@@ -100,7 +119,7 @@ public class Client {
     }
   }
 
-  void parseJobInfo(String[] jobinfo){
+  void parseJobInfo(String[] jobinfo) {
     try {
       jobID = Integer.parseInt(jobinfo[2]);
       reqCore = Integer.parseInt(jobinfo[4]);
@@ -108,13 +127,13 @@ public class Client {
       reqDisk = Integer.parseInt(jobinfo[6]);
     } catch (ArrayIndexOutOfBoundsException e) {
       System.out.println("ArrayIndexOutOfBoundsException ==> " + e.getMessage());
-    }  catch (NumberFormatException e) {
+    } catch (NumberFormatException e) {
       // TODO: handle exception
     }
 
   }
 
-  void parseServerInfo(String[] jobInfo){
+  void parseServerInfo(String[] jobInfo) {
 
   }
 
