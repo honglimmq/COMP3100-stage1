1d35498f477c06003a4a05a30433725d07fbf905
Moon <hong.lim@students.mq.edu.au>
2023-04-01 21:49:35 +1100

    Client.java -Changed logic such that it would Only send GETS command once instead  of everytime JOBN/JOBP was received -Added S1testConfigs test files and bash script for testing, and  verification

--- Client.java
+++ Client.java
@@ -7,11 +7,9 @@ enum Command {
   HELO, AUTH, REDY, OK, GETS, SCHD, ENQJ, DEQJ, LSTQ, CNTJ, EJWT, LSTJ, MIGJ, KILJ, TERM, QUIT
 }
 
-
 enum ServerCommand {
   DATA, JOBN, JOBP, JCPL, RESF, RESR, CHKQ, NONE, ERR, OK, QUIT
 }
-
 
 public class Client {
   private final String EMPTYSTRING = "";
@@ -21,13 +19,13 @@ public class Client {
 
   boolean debug = true;
   int count = 0;
+  boolean firstPass = true;
 
   Socket socket;
   DataOutputStream out;
   BufferedReader in;
   String incomingMsg = EMPTYSTRING;
   String outgoingMsg = EMPTYSTRING;
-
 
   // job information
   int jobID = 0;
@@ -36,7 +34,6 @@ public class Client {
   int reqDisk = 0;
 
   // server information
-
 
   public void run() throws IOException {
     // Connect
@@ -81,38 +78,45 @@ public class Client {
     socket.close();
   }
 
-  void handleJob(String jobInfo[]) throws IOException{
+  void handleJob(String jobInfo[]) throws IOException {
     parseJobInfo(jobInfo);
-    // Generate outgoing message for GETS command
-    outgoingMsg = "Capable" + WHITESPACE + reqCore + WHITESPACE + reqMemory + WHITESPACE + reqDisk;
-    sendMsg(Command.GETS, outgoingMsg);
-    incomingMsg = recvMsg();
 
-    String[] spiltedMsg = incomingMsg.split("\\s++");
-    // recv DATA jobID
-    int numOfServer = Integer.parseInt(spiltedMsg[1]);
-    int maxCore = -1;
-    int serverID = -1;
-    String serverType = EMPTYSTRING;
-
-
-    sendMsg(Command.OK);
-
-    for (int i = 0; i < numOfServer; i++) {
+    if (firstPass) {
+      // Generate outgoing message for GETS All command
+      sendMsg(Command.GETS, "All");
       incomingMsg = recvMsg();
-      spiltedMsg = incomingMsg.split("\\s++");
 
-      int core = Integer.parseInt(spiltedMsg[4]);
-      if (maxCore < core) {
-        serverType = spiltedMsg[0];
-        serverID = Integer.parseInt(spiltedMsg[1]);
-        maxCore = core;
+      // Determine jobID to schedule a server for
+      String[] spiltedMsg = incomingMsg.split("\\s++");
+      
+      int numOfServer = Integer.parseInt(spiltedMsg[1]);
+      int maxCore = -1;
+      int serverID = -1;
+      String serverType = EMPTYSTRING;
+
+      sendMsg(Command.OK);
+
+      // Identifying the largest server type based on core 
+      // and count how many of that server type are there. 
+      // State information on each server is formmated as:
+      // [serverType] [serverID] [state] [currStartTime] [core] [memory] [disk]
+      for (int i = 0; i < numOfServer; i++) {
+        incomingMsg = recvMsg();
+        spiltedMsg = incomingMsg.split("\\s++");
+
+        int core = Integer.parseInt(spiltedMsg[4]);
+        if (maxCore < core) {
+          serverType = spiltedMsg[0];
+          serverID = Integer.parseInt(spiltedMsg[1]);
+          maxCore = core;
+        }
       }
+
+      sendMsg(Command.OK);
+      incomingMsg = recvMsg();
+
+      firstPass = false;
     }
-
-    sendMsg(Command.OK);
-    incomingMsg = recvMsg();
-
     // Schedule a job
     outgoingMsg = jobID + WHITESPACE + serverType + WHITESPACE + serverID;
     sendMsg(Command.SCHD, outgoingMsg);
@@ -132,7 +136,6 @@ public class Client {
   void sendMsg(Command cmd) throws IOException {
     sendMsg(cmd, EMPTYSTRING);
   }
-
 
   void sendMsg(Command cmd, String parameters) throws IOException {
     String message;
@@ -168,7 +171,6 @@ public class Client {
   void parseServerInfo(String[] jobInfo) {
 
   }
-
 
   public static void main(String args[]) throws Exception {
     new Client().run();
